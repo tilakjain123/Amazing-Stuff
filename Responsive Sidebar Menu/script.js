442c387e8e52b58633290c5d28bf5212885d37b2
Tilak Jain <89719256+tilakjain123@users.noreply.github.com>
2023-10-29 13:27:14 +0530

    Create script.js

---
+++ Responsive Sidebar Menu/script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body"),
+  sidebar = body.querySelector(".sidebar"),
+  toggle = body.querySelector(".toggle"),
+  searchBtn = body.querySelector(".search-box"),
+  modeSwitch = body.querySelector(".mode"),
+  modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
